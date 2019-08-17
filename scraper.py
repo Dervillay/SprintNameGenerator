122e8b3d69481f5ae1b742e80d563e71897eaffe
Dervillay <james.gascoigneburns7@gmail.com>
2019-08-17 18:34:31 +0100

    Add function for scraping book titles and associated food and drink

--- scraper.py
+++ scraper.py
@@ -1,31 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
-import re
 
-bookURL = 'http://www.fiction-food.com/p/banquet-of-books.html'
-content = requests.get(bookURL)
-
-soup = BeautifulSoup(content.text, 'html.parser')
-
-# Functions need sanitising
 def find_books():
-    responses = []
+    URL = 'http://www.fiction-food.com/p/banquet-of-books.html'
+    content = requests.get(URL)
+
+    soup = BeautifulSoup(content.text, 'html.parser')
+
+    responses = {}
+
     for line in soup.find_all('b'):
+
         if '<b><i>' in str(line):
-            responses.append(str(line).replace('<b>', '')
-                                      .replace('</b>', '')
-                                      .replace('<i>', '')
-                                      .replace('</i>', ''))
+            foodsAndDrinks = []
+            currentFoodOrDrink = line.find_next_sibling('a')
+
+            while '<b><i>' not in str(currentFoodOrDrink):
+                if '<a href' in str(currentFoodOrDrink):
+                    foodsAndDrinks.append(currentFoodOrDrink.text.strip().encode('utf-8'))
+                if currentFoodOrDrink != None:
+                    currentFoodOrDrink = currentFoodOrDrink.next_sibling
+                else:
+                    break
+
+            responses[line.text.strip().encode('utf-8')] = foodsAndDrinks
+
     return responses
 
-def find_food_and_drink():
-    responses = []
-    for line in soup.find_all('a'):
-        if '<a href' in str(line) and line.string != None:
-            if len(line.string) > 3:
-                responses.append(line.text.strip().encode('utf-8'))
-    return responses[8:]
-
-
 books = find_books()
-foodAndDrink = find_food_and_drink()
+
+for x in books:
+    print (x)
+    for y in books[x]:
+        print (':' + y)
